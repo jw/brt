14a832a3fddcf2559d95d51851d6733b5f5a8c45
Jan Willems <jw@elevenbits.com>
2024-02-22 22:34:21 +0100

    feat: Added list of processes on the terminal: pid, ppid, command, number_of_threads, uid, mem, cpu (tbd).
    Signed-off-by: Jan Willems <jw@elevenbits.com>

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,19 @@ use std::{
     time::Duration,
 };
 
-use chrono::{DateTime, Utc};
 use clap::Parser;
 use crossterm::{
     event::{self, Event, KeyCode},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use log::info;
+use log::{info, warn};
+use procfs::process::{all_processes, Process};
+use ratatui::layout::Constraint::Percentage;
+use ratatui::widgets::{Cell, Row, Table};
 use ratatui::{
     prelude::*,
-    widgets::{block::Title, Block, BorderType, Borders, Padding, Paragraph},
+    widgets::{block::Title, Block, BorderType, Borders, Padding},
 };
 
 mod logger;
@@ -35,6 +37,7 @@ struct Cli {}
 
 fn main() -> Result<()> {
     logger::initialize_logging();
+    initialize_panic_handler();
     info!("{NAME} ({VERSION}) started.");
 
     let _cli = Cli::parse();
@@ -84,28 +87,94 @@ fn handle_events() -> Result<ControlFlow<()>> {
     Ok(ControlFlow::Continue(()))
 }
 
+pub fn initialize_panic_handler() {
+    let original_hook = std::panic::take_hook();
+    std::panic::set_hook(Box::new(move |panic_info| {
+        crossterm::execute!(std::io::stderr(), crossterm::terminal::LeaveAlternateScreen).unwrap();
+        crossterm::terminal::disable_raw_mode().unwrap();
+        original_hook(panic_info);
+    }));
+}
+
 fn ui(frame: &mut Frame) {
     let layout = Layout::default()
         .direction(Direction::Vertical)
         .constraints([Constraint::Percentage(100)])
         .split(frame.size());
 
-    // get the model
-    let now: DateTime<Utc> = Utc::now();
-    let battery = model::get_battery();
-    model::get_processes();
+    let mut rows = Vec::new();
+    let all_processes: Vec<Process> = all_processes()
+        .expect("Can't read /proc")
+        .filter_map(|p| match p {
+            Ok(p) => Some(p),
+            Err(e) => match e {
+                procfs::ProcError::NotFound(_) => None,
+                procfs::ProcError::Io(_e, _path) => None,
+                x => {
+                    println!("Can't read process due to error {x:?}");
+                    None
+                }
+            },
+        })
+        .collect();
+    for process in all_processes {
+        let mut cells = Vec::new();
+        let stat_result = process.stat();
+        match stat_result {
+            Ok(stat) => {
+                let pid = Cell::new(stat.pid.to_string());
+                cells.push(pid);
+                let ppid = Cell::new(stat.ppid.to_string());
+                cells.push(ppid);
+                let command = Cell::new(stat.comm);
+                cells.push(command);
+                let number_of_threads = Cell::new(stat.num_threads.to_string());
+                cells.push(number_of_threads);
+                let uid_result = process.uid();
+                match uid_result {
+                    Ok(user) => {
+                        let user = Cell::new(user.to_string());
+                        cells.push(user);
+                    }
+                    Err(_e) => {
+                        warn!("Nu user found for {}", process.pid().to_string());
+                        break;
+                    }
+                }
+                let mem = Cell::new(stat.vsize.to_string());
+                cells.push(mem);
+                let cpu = Cell::new("n/a".to_string());
+                cells.push(cpu);
+            }
+            Err(_e) => {
+                warn!("Stat not found for {}", process.pid().to_string());
+                break;
+            }
+        }
+        rows.push(Row::new(cells));
+    }
 
-    let block = Block::default()
+    info!("Battery: {}", model::get_battery());
+
+    let _block = Block::default()
         .title(Title::from("brt").alignment(Alignment::Center))
         .padding(Padding::new(0, 0, frame.size().height / 2 - 1, 0))
         .borders(Borders::ALL)
         .border_style(Style::default().fg(Color::White))
         .border_type(BorderType::Rounded);
-    let paragraph =
-        Paragraph::new(Line::from(now.to_string() + " | " + battery.as_str()).centered())
-            .block(block);
 
-    frame.render_widget(paragraph, layout[0]);
+    let widths = [
+        Percentage(5),
+        Percentage(5),
+        Percentage(70),
+        Percentage(5),
+        Percentage(5),
+        Percentage(5),
+        Percentage(5),
+    ];
+    let table = Table::new(rows, widths);
+
+    frame.render_widget(table, layout[0]);
 }
 
 #[cfg(test)]
