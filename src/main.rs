a824e542bfa676bae18992c8b0783d0558bff8d0
jw <jw@elevenbits.com>
2025-04-09 23:53:23 +0200

    :sparkles: Replace the octocrab by battery information.

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,11 @@ use std::time::Duration;
 
 use color_eyre::Result;
 use crossterm::event::{Event, EventStream, KeyCode, KeyEventKind};
-use octocrab::params::pulls::Sort;
-use octocrab::params::Direction;
-use octocrab::Page;
 use ratatui::buffer::Buffer;
 use ratatui::layout::{Constraint, Layout, Rect};
-use ratatui::style::{Style, Stylize};
+use ratatui::style::Stylize;
 use ratatui::text::Line;
-use ratatui::widgets::{Block, HighlightSpacing, Row, StatefulWidget, Table, TableState, Widget};
+use ratatui::widgets::{Paragraph, Row, TableState, Widget};
 use ratatui::{DefaultTerminal, Frame};
 use tokio_stream::StreamExt;
 
@@ -83,7 +80,7 @@ struct PullRequestListWidget {
 
 #[derive(Debug, Default)]
 struct PullRequestListState {
-    pull_requests: Vec<PullRequest>,
+    pull_requests: String,
     loading_state: LoadingState,
     table_state: TableState,
 }
@@ -101,7 +98,7 @@ enum LoadingState {
     Idle,
     Loading,
     Loaded,
-    Error(String),
+    // Error(String),
 }
 
 impl PullRequestListWidget {
@@ -118,30 +115,25 @@ impl PullRequestListWidget {
         // this runs once, but you could also run this in a loop, using a channel that accepts
         // messages to refresh on demand, or with an interval timer to refresh every N seconds
         self.set_loading_state(LoadingState::Loading);
-        match octocrab::instance()
-            .pulls("ratatui", "ratatui")
-            .list()
-            .sort(Sort::Updated)
-            .direction(Direction::Descending)
-            .send()
-            .await
-        {
-            Ok(page) => self.on_load(&page),
-            Err(err) => self.on_err(&err),
+        let manager = battery::Manager::new().expect("Could not get battery");
+        let mut battery_state = "".to_string();
+        for (idx, maybe_battery) in manager.batteries().expect("No battery found").enumerate() {
+            let battery = maybe_battery.expect("No battery found");
+            battery_state.push_str(format!("index #{}", idx).as_str());
+            battery_state.push_str(format!(" Vendor: {:?}", battery.vendor()).as_str());
+            battery_state.push_str(format!(" Model: {:?}", battery.model()).as_str());
+            battery_state.push_str(format!(" State: {:?}", battery.state()).as_str());
+            battery_state.push_str(format!(" Time to full charge: {:?}", battery.time_to_full()).as_str());
+            battery_state.push_str(format!(" serial: {:?}", battery.serial_number()).as_str());
+            battery_state.push_str(format!(" energy: {:?}", battery.energy()).as_str());
+            battery_state.push_str(format!(" energy: {:?}", battery.energy_full_design()).as_str());
         }
+        self.on_load(&battery_state)
     }
-    fn on_load(&self, page: &Page<OctoPullRequest>) {
-        let prs = page.items.iter().map(Into::into);
+    fn on_load(&self, page: &str) {
         let mut state = self.state.write().unwrap();
         state.loading_state = LoadingState::Loaded;
-        state.pull_requests.extend(prs);
-        if !state.pull_requests.is_empty() {
-            state.table_state.select(Some(0));
-        }
-    }
-
-    fn on_err(&self, err: &octocrab::Error) {
-        self.set_loading_state(LoadingState::Error(err.to_string()));
+        state.pull_requests = page.to_string();
     }
 
     fn set_loading_state(&self, state: LoadingState) {
@@ -157,47 +149,27 @@ impl PullRequestListWidget {
     }
 }
 
-type OctoPullRequest = octocrab::models::pulls::PullRequest;
-
-impl From<&OctoPullRequest> for PullRequest {
-    fn from(pr: &OctoPullRequest) -> Self {
-        Self {
-            id: pr.number.to_string(),
-            title: pr.title.as_ref().unwrap().to_string(),
-            url: pr
-                .html_url
-                .as_ref()
-                .map(ToString::to_string)
-                .unwrap_or_default(),
-        }
-    }
-}
+// type OctoPullRequest = octocrab::models::pulls::PullRequest;
+//
+// impl From<&OctoPullRequest> for PullRequest {
+//     fn from(pr: &OctoPullRequest) -> Self {
+//         Self {
+//             id: pr.number.to_string(),
+//             title: pr.title.as_ref().unwrap().to_string(),
+//             url: pr
+//                 .html_url
+//                 .as_ref()
+//                 .map(ToString::to_string)
+//                 .unwrap_or_default(),
+//         }
+//     }
+// }
 
 impl Widget for &PullRequestListWidget {
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let mut state = self.state.write().unwrap();
-
-        // a block with a right aligned title with the loading state on the right
-        let loading_state = Line::from(format!("{:?}", state.loading_state)).right_aligned();
-        let block = Block::bordered()
-            .title("Pull Requests")
-            .title(loading_state)
-            .title_bottom("j/k to scroll, q to quit");
-
-        // a table with the list of pull requests
-        let rows = state.pull_requests.iter();
-        let widths = [
-            Constraint::Length(5),
-            Constraint::Fill(1),
-            Constraint::Max(49),
-        ];
-        let table = Table::new(rows, widths)
-            .block(block)
-            .highlight_spacing(HighlightSpacing::Always)
-            .highlight_symbol(">>")
-            .row_highlight_style(Style::new().on_blue());
-
-        StatefulWidget::render(table, area, buf, &mut state.table_state);
+        let state = self.state.write().unwrap();
+        let p = Paragraph::new(state.pull_requests.as_str());
+        Widget::render(p, area, buf);
     }
 }
 
