16f87409c36b1ad5bef048a354e9a8e6dfbe5b43
jw <jw@elevenbits.com>
2025-04-13 20:27:51 +0200

    :recycle: Clean the main module.

--- src/main.rs
+++ src/main.rs
@@ -13,21 +13,3 @@ async fn main() -> Result<()> {
     ratatui::restore();
     result
 }
-
-
-// type OctoPullRequest = octocrab::models::pulls::PullRequest;
-//
-// impl From<&OctoPullRequest> for PullRequest {
-//     fn from(pr: &OctoPullRequest) -> Self {
-//         Self {
-//             id: pr.number.to_string(),
-//             title: pr.title.as_ref().unwrap().to_string(),
-//             url: pr
-//                 .html_url
-//                 .as_ref()
-//                 .map(ToString::to_string)
-//                 .unwrap_or_default(),
-//         }
-//     }
-// }
-
