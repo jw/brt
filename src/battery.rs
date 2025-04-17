bdfeadc3984395362d59233900144b929d2cdca1
jw <jw@elevenbits.com>
2025-04-17 19:15:16 +0200

    :recycle: Rename BrtBattery to BatteryState.

--- src/battery.rs
+++ src/battery.rs
@@ -2,6 +2,7 @@ use std::error::Error;
 use std::time::Duration;
 use std::sync::{Arc, RwLock};
 use std::fmt;
+use std::string::ToString;
 use battery::State;
 use battery::units::{Energy, Ratio, Time};
 use ratatui::prelude::Widget;
@@ -14,13 +15,8 @@ pub struct BatteryWidget {
     state: Arc<RwLock<BatteryState>>,
 }
 
-#[derive(Debug, Default)]
-struct BatteryState {
-    battery: BrtBattery,
-}
-
 #[derive(Debug, Default, Clone, Copy)]
-struct BrtBattery {
+struct BatteryState {
     state_of_health: Ratio,
     time_to_empty: Option<Time>,
     time_to_full: Option<Time>,
@@ -28,7 +24,7 @@ struct BrtBattery {
     state: State,
 }
 
-impl fmt::Display for BrtBattery {
+impl fmt::Display for BatteryState {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", self.state)
     }
@@ -44,19 +40,19 @@ impl BatteryWidget {
     async fn battery(self) {
         let mut interval = tokio::time::interval(Duration::from_millis(100));
         loop {
-            let mut brt_battery = BrtBattery::default();
+            let mut state = BatteryState::default();
             {
                 let manager = battery::Manager::new().expect("Failed to init battery manager");
                 {
                     for (_, maybe_battery) in manager.batteries().expect("No battery found").enumerate() {
                         let battery = maybe_battery.expect("No battery found");
-                        brt_battery.state_of_health = battery.state_of_health();
-                        brt_battery.time_to_empty = battery.time_to_empty();
-                        brt_battery.time_to_full = battery.time_to_full();
-                        brt_battery.energy = battery.energy();
-                        brt_battery.state = battery.state();
+                        state.state_of_health = battery.state_of_health();
+                        state.time_to_empty = battery.time_to_empty();
+                        state.time_to_full = battery.time_to_full();
+                        state.energy = battery.energy();
+                        state.state = battery.state();
                     }
-                    self.on_load(&brt_battery);
+                    self.on_load(&state);
                 }
             }
             interval.tick().await;
@@ -64,9 +60,11 @@ impl BatteryWidget {
         }
     }
 
-    fn on_load(&self, battery: &BrtBattery) {
+    fn on_load(&self, battery_state: &BatteryState) {
         let mut state = self.state.write().unwrap();
-        state.battery = *battery;
+        state.state = battery_state.state;
+        state.state_of_health = battery_state.state_of_health;
+
     }
 
     pub fn scroll_down(&self) {
@@ -76,10 +74,26 @@ impl BatteryWidget {
     }
 }
 
+static BATTERY_STATE_SYMBOL_UNKNOWN: &str = "?";
+static BATTERY_STATE_SYMBOL: &[(State, &str)] = &[
+        (State::Charging, "▲"),
+        (State::Discharging, "▼"),
+        (State::Full, "●"),
+        (State::Empty, "○"),
+        (State::Unknown, BATTERY_STATE_SYMBOL_UNKNOWN),
+    ];
+
+fn get_state_symbol(s: State) -> String {
+    if let Some((_, symbol)) = BATTERY_STATE_SYMBOL.iter().find(|(state, _)| s == *state) {
+        return symbol.to_string();
+    };
+    BATTERY_STATE_SYMBOL_UNKNOWN.to_string()
+}
+
 impl Widget for &BatteryWidget {
     fn render(self, area: Rect, buf: &mut Buffer) {
         let state = self.state.write().unwrap();
-        let p = Paragraph::new(format!("Battery state: {}", state.battery));
+        let p = Paragraph::new(format!("BAT{} {:?} ==== xyz", get_state_symbol(state.state), state.state_of_health));  // , state.state_of_health));
         Widget::render(p, area, buf);
     }
 }
